491830b178e11071190cd762635c9d1ffa700051
“Veronica <vero.parragraca@gmail.com>
2024-02-12 00:16:14 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let arrayVacio = [];
+let arrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arrayNumerosPares = [0, 2, 4, 6, 8];
+let arrayBidimensional = [
+  [0, 1, 2],
+  ["a", "b", "c"],
+];
+
+let suma = (a, b) => a + b;
+let potenciacion = (a, b) => a ** b;
